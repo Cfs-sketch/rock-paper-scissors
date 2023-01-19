1221135b3e98ef40bd12f3d11cad4b25a28bf8bb
alejandroflores <alexf2620@gmail.com>
2023-01-19 08:50:23 -0800

    Update main.js
    add some variables, functions, andconditionals.

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+
+const playerSelector = ('rock', 'paper', 'scissors','Rock'
+, 'Paper', 'Scissors', 'ROCK', 'PAPER', 'SCISSORS');
+computerSelector = getComputerChoice();
+console.log(playRound(playerSelect, computerSelector));
+
 function getComputerChoice(Rock, Paper, Scissors) {
     Rock = 'Rock'; Paper = 'Paper'; Scissors = 'Scissors';
 return Math.floor(Math.random(Rock, Paper, Scissors));
 }
 
-
 function playRound(playerSelect, computerSelect) {
-//
-}
+
+    if (playerSelect === computerSelect) {
+        alert('It\'s a tie!');
+    } else if (playerSelect === 'Rock') 
+        if (computerSelect === 'Scissors') {
+            alert('You win!');
+}}
