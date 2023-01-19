8d5a7ffd3300f3fab2bed70f5e08989b149fc7a6
alejandroflores <alexf2620@gmail.com>
2023-01-19 07:25:57 -0800

    created two function in mian,js

---
+++ main.js
@@ -0,0 +1,9 @@
+function getComputerChoice(Rock, Paper, Scissors) {
+    Rock = 'Rock'; Paper = 'Paper'; Scissors = 'Scissors';
+return Math.floor(Math.random(Rock, Paper, Scissors));
+}
+
+
+function playRound(playerSelect, computerSelect) {
+//
+}
