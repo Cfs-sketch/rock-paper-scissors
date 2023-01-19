571d1b7c5b0a27490b149fbbc6ae0d5967c0f63c
alejandroflores <alexf2620@gmail.com>
2023-01-19 10:26:39 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,21 @@
 const playerSelector = ('rock', 'paper', 'scissors','Rock'
 , 'Paper', 'Scissors', 'ROCK', 'PAPER', 'SCISSORS');
 computerSelector = getComputerChoice();
-console.log(playRound(playerSelect, computerSelector));
+console.log(playRound(playerSelector, computerSelector));
 
 function getComputerChoice(Rock, Paper, Scissors) {
     Rock = 'Rock'; Paper = 'Paper'; Scissors = 'Scissors';
 return Math.floor(Math.random(Rock, Paper, Scissors));
 }
 
-function playRound(playerSelect, computerSelect) {
+function playRound(playerSelector, computerSelector) {
 
-    if (playerSelect === computerSelect) {
+    if (playerSelector === computerSelector) {
         alert('It\'s a tie!');
-    } else if (playerSelect === 'Rock') 
-        if (computerSelect === 'Scissors') {
+    } else if (playerSelector === 'Rock') 
+        if (computerSelector === 'Scissors') {
             alert('You win!');
 }}
+function game(playRound){
+
+}
